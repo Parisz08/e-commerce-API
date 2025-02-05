3bdc753e0ff308981ad2e8ea42f8993019d75a34
parisz08 <muhamadparisz08@gmail.com>
2025-02-05 11:20:09 +0700

    second commit

--- src/components/FilledButton.js
+++ src/components/FilledButton.js
@@ -1,10 +1,19 @@
-export default function FilledButton({type = "button", className, children }) {
-    return (
-        <button
-          type={type}
-          className={"flex items-center justify-center gap-2 px-4 py-3 bg-dark text-white rounded-full font-semibold" + className}
-        >
-          {children}
-        </button>
-    );
+export default function FilledButton({
+  type = "button",
+  className,
+  disabled = false,
+  children,
+}) {
+  return (
+    <button
+      type={type}
+      className={
+        "flex items-center justify-center gap-2 px-4 py-3 bg-dark text-white rounded-full font-semibold" +
+        className
+      }
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 }
